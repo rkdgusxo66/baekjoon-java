cfa422844416c7c2cf7d51d966a28f27d0a84dff
alfie.m <alfie.m@kakaocorp.com>
2020-08-29 19:08:14 +0900

    solved P10000

---
+++ src/main/java/P10000.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P10000 {
+    public static int sum(int x, int y) {
+        return x+y;
+    }
+
+    public static void main(String... argv) {
+        Scanner sc = new Scanner(System.in);
+        int sum = sum(sc.nextInt(), sc.nextInt());
+        System.out.println(sum);
+    }
+}
