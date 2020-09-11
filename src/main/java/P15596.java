a8fd16c86da1deaf6a2e49cb8683214f196cbcdd
alfie.m <alfie.m@kakaocorp.com>
2020-09-12 02:01:21 +0900

    solved P15596

---
+++ src/main/java/P15596.java
@@ -0,0 +1,12 @@
+class Test {
+    long sum(int[] a) {
+        long ans = 0;
+        for(int v: a){
+            ans += v;
+        }
+        return ans;
+    }
+}
+public class P15596 {
+
+}
---
+++ src/test/java/P15596Test.java
@@ -0,0 +1,5 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class P15596Test {
+}
